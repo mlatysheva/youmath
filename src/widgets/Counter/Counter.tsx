c5dbca2dc46130f22259faebb53302fb1b38d34b
k1nnyyY <osman.haciev231@gmail.com>
2023-02-05 19:29:45 +0400

    feat: add footer

---
+++ src/widgets/Counter/Counter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Counter.module.scss';
+
+interface Props {}
+
+export const Counter: React.FC<Props> = () => {
+  return (
+    <div className={classes.wrapper}>
+      <p>Visitor count: 12345</p> 
+    </div> 
+  );
+};
--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -1,5 +1,22 @@
+import React from 'react';
 import classes from './Footer.module.scss';
+import { Counter } from '../Counter/Counter';
 
 export const Footer = () => {
-  return <footer className={classes.footer}></footer>;
+  return (
+    <div className={classes.booter}>
+      <div className={classes.Copyright}>
+        <p>&copy; 2023 All rights reserved.</p>
+      </div>
+      <div className={classes.Wrapper}>
+        <div className={classes.Terms}> 
+          <a href="#">Terms and Conditions</a> 
+        </div> 
+          <Counter/>
+      </div>
+      <div className={classes.Logo}>
+        <img src="../../shared/assets/images/logo.jpg" alt="Company logo" />
+      </div>
+    </div>
+  );
 };
