7d73c3c5b86c43bc1c9f9c2d3007d1cd217a1065
Yana Morozova <yanabonne@yandex.ru>
2023-02-23 12:44:26 +0300

    fix: fix fonts import

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/styles/index.scss';
 import App from './app/App';
-import "./vendor/fonts.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
