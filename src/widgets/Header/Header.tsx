314ce88350bac43f09a4bda6ef43f161618b785b
mlatysheva <mlatysheva@gmail.com>
2023-03-07 21:22:14 +0100

    feat: add small header with search on other pages

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -3,26 +3,34 @@ import classes from './Header.module.scss';
 import { Search } from '../Search/Search';
 import { randomItems } from './model/randomItems';
 import { LinkItem } from '../../shared/ui/LinkItem/LinkItem';
+import { useLocation } from 'react-router-dom';
+import { BASE_URL } from '../../app/constants/baseUrl';
 
-interface Props {}
+const Header: React.FC = () => {
+  const { pathname } = useLocation();
 
-const Header: React.FC<Props> = () => {
-  return (
-    <div
-      className={classes.header}
-    >      
-      <div className={`${classes.headerMain} wrapper`}>
-        <h1 className={classes.title}>Примеры решения задач <br></br>по высшей математике</h1> 
-        <p className={classes.regular}>Не думай — качай, типовик — сдавай!</p>              
-        <Search />
-        <div className={classes.randomItems}>
-          {randomItems.map((item) => (
-            <LinkItem className={classes.randomLink} key={item.title} item={item} />
-          ))}
+  if (pathname === `/${BASE_URL}` || pathname === `/${BASE_URL}/`) {
+    
+    return (
+      <div
+        className={classes.header}
+      >      
+        <div className={`${classes.headerMain} wrapper`}>
+          <h1 className={classes.title}>Примеры решения задач <br></br>по высшей математике</h1> 
+          <p className={classes.regular}>Не думай — качай, типовик — сдавай!</p>              
+          <Search />
+          <div className={classes.randomItems}>
+            {randomItems.map((item) => (
+              <LinkItem className={classes.randomLink} key={item.title} item={item} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+
+  }
 };
 
 export { Header };
